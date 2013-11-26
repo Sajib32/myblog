31f5d30db1f222ce482a86a697135fb9f01663c8
Monirul Islam <sajibnstu@gmail.com>
2013-11-26 21:40:46 +0600

    Category works done

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,40 +6,35 @@ class CategoriesController < ApplicationController
 	end
 
 	def list
-		@categories = Category.order('categories.id ASC')
-		@category = Category.new
+		@categories = Category.all
+		@category = Category.find(params[:id]) if params[:id]
+		@category = Category.new if @category.nil?
 	end
 
 
 	def create
 		 # Instantiate a new object using form parameters
-		@category = Category.new[:category]
+		@category = Category.new(params[:category])
     	 # Save the object
-    	if @category .save
+    	if @category.save
       	# If save succeeds, redirect to the list action
       		flash[:notice] = "Category created."
       		redirect_to(:action => 'list')
     	else
       	# If save fails, redisplay the form so user can fix problems
-      		render('new')
+      		render('list')
     	end
 	end
 
-	def edit
-		@category = Category.find(params[:id])
-	end
-
 	def update
-		
 		@category = Category.find(params[:id])
-    	
     	if @category.update_attributes(params[:category])
 			flash[:notice] = "Category updated."
       		redirect_to(:action => 'list')
     	else
-      		render('edit')
+      		render('list')
+    	end
     end
-end
 
 	def destroy
 		Category.find(params[:id]).destroy
