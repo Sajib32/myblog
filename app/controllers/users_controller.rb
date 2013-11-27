93b6b3c1484e92b7ae574afbb12d8a4f81ac46a1
Monirul Islam <sajibnstu@gmail.com>
2013-11-27 12:22:16 +0600

    Another technique where create & update is done in one action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,27 +13,32 @@ class UsersController < ApplicationController
 
 	def manage
 		list
-		@user = User.find(params[:id]) if params[:id]
-		@user = User.new if @user.nil?
-	end
-
-	def create
-		@user = User.new(params[:user])
-		if @user.save
+		if request.get? && params[:id].blank? #new
+			@user = User.new
+		elsif request.post? && params[:id].blank?
+			@user = User.new(params[:user])
+		  if @user.save
 			flash[:notice] = "User successfully created."
 			redirect_to(:action => 'list')
-		else
+		  else
 			render('manage')
+		  end
+		elsif request.get? && !params[:id].blank? #edit
+			@user = User.find(params[:id])	
+		elsif request.post? && !params[:id].blank? #update 	
+			@user = User.find(params[:id])
+		   if @user.update_attributes(params[:user])
+			flash[:notice] = "User successfully updated."
+			redirect_to(:action => 'list')
+		   else
+			render('manage')
+		   end
 		end
 	end
 
-	def update
-		@user = User.find(params[:id])
-		if @user.update_attributes(params[:user])
-			flash[:notice] = "User successfully updated."
-			redirect_to(:action => 'list')
-		else
-			render('manage')
-		end
+	def delete
+		User.find(params[:id]).destroy
+		flash[:notice] = "User successfully deleted."
+		redirect_to(:action => 'list')
 	end
 end
