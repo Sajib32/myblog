9683c05d1deeb7afea987a570f07f8217feb006d
Monirul Islam <sajibnstu@gmail.com>
2013-12-02 13:29:15 +0600

    Displaying only approved comments with post.

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,12 +5,17 @@ class Post < ActiveRecord::Base
   			 :foreign_key => 'author_id'
 
   has_many :comments, :order => 'created_at ASC', :dependent => :destroy
- 
+  has_many :approved_comments, :class_name => 'Comment', 
+  		   :conditions => "status = 'approved'"
   has_many :recent_posts, :class_name => 'Post', :order => 
   			'created_at ASC', :limit => 5
 
   has_many :categorizations
 
   has_many :categories, :through => :categorizations
+
+  #def approved_comments
+  	#self.comments.find(:all, :conditions => "status = 'approved")
+  #end
 
 end
